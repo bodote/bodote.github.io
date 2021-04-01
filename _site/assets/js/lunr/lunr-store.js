a40373954485cb3dab8c60f5776c14cce4bde9e3
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2021-04-01 10:30:25 +0200

    .

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,36 +2,36 @@ var store = [{
         "title": "(Video)Test Driven Development und Test-First Tutorial",
         "excerpt":"Nach wie vor aktuell: Mein Tutorial zu Test Driven Development und Test-First               ","categories": ["blog"],
         "tags": ["TDD","Test Driven Development"],
-        "url": "http://localhost:4000/blog/TDD-Tutorial/",
+        "url": "/blog/TDD-Tutorial/",
         "teaser": null
       },{
         "title": "Sehr große Datenbanken sehr schnell kopieren",
         "excerpt":"Sehr große produktive Datenbanken als schreibbare Test-DBs den Entwicklern in Sekunden zu Verfügung stellen Das Problem Im Rahmen einer großen Webapps-Entwicklung braucht das SW-Entwicklerteam zum Debuggen die “echten” Daten der Datenbank. Wenn diese aber leider mehrere 100 Gigabyte oder mehrere Terabyte groß ist, führt das zu sehr langen Wartezeiten in...","categories": ["blog"],
         "tags": ["Testing","DevOps"],
-        "url": "http://localhost:4000/blog/Big-DB-Copy-in-Seconds/",
+        "url": "/blog/Big-DB-Copy-in-Seconds/",
         "teaser": null
       },{
         "title": "TDD? aber richtig!",
         "excerpt":"Warum »TDD« und »Test First« ? Vorweg eine Frage: Sind wir uns eigentlich einig, dass Unittests mit angemessener Testabdeckung sehr wünschenswert sind ? (Wobei es mir hier tatsächlich erstmal nur um Unittests geht, nicht um Integrationstests oder um Akzeptanztests oder der gleichen) Wenn ja: Gut, dann haben wir diese Hürde ja schon mal aus...","categories": ["blog"],
         "tags": ["TDD","Test Driven Development"],
-        "url": "http://localhost:4000/blog/TDD-aber-richtig/",
+        "url": "/blog/TDD-aber-richtig/",
         "teaser": null
       },{
         "title": "Literaturrecherche zu TDD",
         "excerpt":"Bücher: Ich bin ja ein enthusiastischer Befürworter von Test Driven Design (TDD), insbesondere von dem “test first” Ansatz. Daher hat es mich einfach interessiert, was denn andere Autoren und auch (aber nicht nur) die wissenschaftliche Literatur zu dem Thema hergibt. Hier ist, was sich dazu gefunden habe:1 Aus Langlebige Softwarearchitekturen...","categories": ["blog"],
         "tags": ["TDD","Test Driven Development"],
-        "url": "http://localhost:4000/blog/Literaturrecherche-TDD/",
+        "url": "/blog/Literaturrecherche-TDD/",
         "teaser": null
       },{
         "title": "Test Driven Development mit Angular Folge 1-5",
         "excerpt":"Angular TDD Workshop Diese Angular-TDD-Workshop ist eine Variante von diesem Workshop Wir verwenden hier aber jasmine statt Jest Setup git clone https://github.com/bodote/tdd-angular.git git checkout v1.0 oder verwende ng new tdd-angular und reduziere dann src/app/app.component.html auf ein Minimum , lösche alles und setzte &lt;div class=\"content\"&gt; &lt;span&gt; app is running!&lt;/span&gt; &lt;/div&gt; ein....","categories": ["blog"],
         "tags": ["TDD","Test Driven Development"],
-        "url": "http://localhost:4000/blog/TDD-mit-Angular/",
+        "url": "/blog/TDD-mit-Angular/",
         "teaser": null
       },{
-        "title": "Test Driven Development mit Angular Folge 6-9",
-        "excerpt":"Angular TDD Workshop Diese Angular-TDD-Workshop ist eine Variante von diesem Workshop Wir verwenden hier aber jasmine statt Jest Aufgabe 6 Legen wir unsere FavoriteMovieComponent zur Seite und widmen uns jetzt dem FavoriteMovieService. Services sind in Angular in der Regel mit dem HttpClientModule und dessen HttpClient implementiert. Hilfreiche Hinweise: Passend zu...","categories": ["blog"],
+        "title": "Test Driven Development mit Angular Folge 6-10",
+        "excerpt":"Angular TDD Workshop Diese Angular-TDD-Workshop ist eine Variante von diesem Workshop Wir verwenden hier aber jasmine statt Jest Hier geht’s zu den ersten 5 Folgen Aufgabe 6 Legen wir unsere FavoriteMovieComponent zur Seite und widmen uns jetzt dem FavoriteMovieService. Services sind in Angular in der Regel mit dem HttpClientModule und...","categories": ["blog"],
         "tags": ["TDD","Test Driven Development"],
-        "url": "http://localhost:4000/blog/TDD-mit-Angular6-9/",
+        "url": "/blog/TDD-mit-Angular6-10/",
         "teaser": null
       }]
---
+++ _site/update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "update the lockfile by running bundle update (or)"
+#bundle update
+echo "delete and then respawn the lockfile by running bundle install"
+# rm Gemfile.lock
+# bundle install
